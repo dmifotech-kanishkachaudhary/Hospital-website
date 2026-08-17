0d58c508265ad3330fed49a19e74039a1cce24b6
Kanishka Choudhary <kanishka27july@gmail.com>
2026-08-17 16:25:19 +0530

    update CORS for Netlify

--- backend/server.js
+++ backend/server.js
@@ -62,13 +62,25 @@ app.get("/", (req, res) => {
 // SOCKET.IO
 // =======================================
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://city-hospitall.netlify.app/"
+];
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
+
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: allowedOrigins,
     methods: ["GET", "POST"],
+    credentials: true,
   },
 });
-
 // Optional
 const onlineUsers = {};
 
